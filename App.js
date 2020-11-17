b82d7d95a33fc5f27dfe8b1065c2fac62b959126
Vaibhav Singh <59394309+vaibhavsingh2@users.noreply.github.com>
2020-11-17 21:12:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+  
+import React from "react";
+
+import ImagePickerExample from "./screens/camera";
+
+export default class App extends React.Component {
+  render() {
+    return <ImagePickerExample />;
+  }
+}
